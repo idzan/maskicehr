68f0339ec1cee96bee50a941b63e5293fc960b20
Marko Idzan <marko.idzan@gmail.com>
2018-02-25 22:33:20 +0100

    Imports Optimization
    And some unecessary files are added to .gitignore ...

--- app/src/main/java/hr/maskice/maskicehr/WebLoader.java
+++ app/src/main/java/hr/maskice/maskicehr/WebLoader.java
@@ -10,7 +10,6 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
